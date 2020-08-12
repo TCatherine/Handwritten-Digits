8a5eaedebbc775d267041a72b3befd64501864c1
safary1094 <safary1094@yandex.ru>
2020-08-12 09:42:12 +0300

    add gpu support

--- Visualization.py
+++ Visualization.py
@@ -3,22 +3,23 @@ import numpy as np
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 
-digits=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 
 def convert(model, X):
-    ProbYNon, ProbY=model.net(torch.FloatTensor(X))
-    ProbY = ProbY.detach().numpy()
+    ProbY = model.inference(X)
     ProbY = np.ndarray.tolist(ProbY)
     y_pred = []
     for y in ProbY:
-         num = y.index(max(y))
-         y_pred.append(digits[num])
+        num = y.index(max(y))
+        y_pred.append(digits[num])
     return y_pred
 
-def ConMatrix(ax, model, X, Y,  name):
+
+def ConMatrix(ax, model, X, Y, name):
     y_pred = convert(model, X)
     cm = confusion_matrix(Y, y_pred)
-    cm=cm/len(Y)
+    cm = cm / len(Y)
     disp = ConfusionMatrixDisplay(cm, display_labels=digits)
     disp.plot(ax=ax, cmap='Blues', values_format='.1f')
     plt.sca(ax)
@@ -30,31 +31,34 @@ def ConMatrix(ax, model, X, Y,  name):
     disp.ax_.set_ylabel('')
     return disp
 
+
 def PlotConfusionMatrix(model, train_x, train_y, test_x, test_y):
     f, axes = plt.subplots(1, 2, figsize=(8, 4))
     f.suptitle('Confusion matrices', fontsize=16)
-    D=ConMatrix(axes[0], model, train_x, train_y, 'Training')
+    D = ConMatrix(axes[0], model, train_x, train_y, 'Training')
     D.ax_.set_ylabel('True label')
-    D=ConMatrix(axes[1], model, test_x, test_y, 'Testing')
+    D = ConMatrix(axes[1], model, test_x, test_y, 'Testing')
     plt.subplots_adjust(left=0.15, wspace=0.4, hspace=0.01)
     f.text(0.4, 0.02, 'Predicted label', ha='left')
     f.colorbar(D.im_, ax=axes)
-    #plt.show()
+    # plt.show()
     return
+
 
 def SearchImage(model, X, Y, N, prediction):
     y_pred = convert(model, X)
-    images=[]
-    Y=Y.tolist()
+    images = []
+    Y = Y.tolist()
     for digit in digits:
-        i=0
-        while Y.count(digit)!=0 and i<N:
+        i = 0
+        while Y.count(digit) != 0 and i < N:
             ind = Y.index(digit)
-            if (y_pred[ind] == Y[ind]) and prediction or (y_pred[ind] != Y[ind]) and not(prediction):
-                i+=1
+            if (y_pred[ind] == Y[ind]) and prediction or (y_pred[ind] != Y[ind]) and not (prediction):
+                i += 1
                 images.append(np.asarray(X[ind].reshape(28, 28)).squeeze())
-            Y[ind]=-1
+            Y[ind] = -1
     return images
+
 
 def PlotExamples(model, X, Y, N, prediction):
     images = SearchImage(model, X, Y, N, prediction)
@@ -64,13 +68,14 @@ def PlotExamples(model, X, Y, N, prediction):
     else:
         f.suptitle('False Prediction', fontsize=16)
 
-    i=1
+    i = 1
     for image in images:
         ax = f.add_subplot(10, N, i)
         ax.set_xticks([])
         ax.set_yticks([])
         plt.imshow(image)
-        i+=1
+        i += 1
+
 
 def ShowExamples(model, X, Y, N):
     PlotExamples(model, X, Y, N, True)
--- main.py
+++ main.py
@@ -4,29 +4,32 @@ import pandas as pd
 import numpy as np
 import torch
 from Model import Model
+import time
 from Visualization import PlotConfusionMatrix, convert, ShowExamples
+
 image_size = 28
 from sklearn.metrics import confusion_matrix
 
-digits=['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
 
 def read_data():
-    files_images=[]
+    files_images = []
     files_images.append('train-images-idx3-ubyte.gz')
     files_images.append('t10k-images-idx3-ubyte.gz')
-    files_labels=[]
+    files_labels = []
     files_labels.append('train-labels-idx1-ubyte.gz')
     files_labels.append('t10k-labels-idx1-ubyte.gz')
-    images=[]
-    list_labels=[]
+    images = []
+    list_labels = []
     for file_images, file_labels in zip(files_images, files_labels):
-        f = gzip.open(file_images,'r')
+        f = gzip.open(file_images, 'r')
         f.read(16)
         buf = f.read()
         f.close()
-        num_images=sys.getsizeof(buf) // (image_size * image_size)
+        num_images = sys.getsizeof(buf) // (image_size * image_size)
         data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
-        X = data.reshape(num_images, image_size*image_size) / 255
+        X = data.reshape(num_images, image_size * image_size) / 255
         images.append(X)
 
         f = gzip.open(file_labels, 'r')
@@ -38,28 +41,32 @@ def read_data():
         list_labels.append(labels)
     return images[0], list_labels[0], images[1], list_labels[1]
 
+
 def accuary(model, x, y, name):
     loss = []
     size_batch = 1024
-    dummies_y=pd.get_dummies(y)
+    dummies_y = pd.get_dummies(y)
     for fnum in range(0, len(dummies_y), size_batch):
-        temp_loss=model.Loss(x[fnum:fnum + size_batch],  dummies_y[fnum:fnum + size_batch])
-        loss.append(temp_loss.detach().numpy())
-    y_pred=convert(model, x)
+        temp_loss = model.loss(x[fnum:fnum + size_batch], dummies_y[fnum:fnum + size_batch])
+        loss.append(temp_loss.cpu().detach().numpy())
+    y_pred = convert(model, x)
     cm = confusion_matrix(y, y_pred)
-    correct=np.trace(cm)
+    correct = np.trace(cm)
     loss = np.array(loss)
     mean_loss = loss.mean()
-    print('%s: Accuracy %d/%d (%.0f%%), Loss %.3f;' % ( name,
-        correct, len(y), 100. * correct / len(y), mean_loss))
+    print('%s: Accuracy %d/%d (%.0f%%), Loss %.3f;' % (name,
+                                                       correct, len(y), 100. * correct / len(y), mean_loss))
     return
 
-def train_loop(model, train_x, train_y, test_x, test_y, max_epochs=300):
+
+def train_loop(model, train_x, train_y, test_x, test_y, max_epochs=50):
     dummies_train_y = pd.get_dummies(train_y)
-    for epoch in range(max_epochs+1):
-        size_batch=1024
+    for epoch in range(max_epochs + 1):
+        t = time.time()
+        size_batch = 1024
         for fnum in range(0, len(dummies_train_y), size_batch):
-            model.Train(train_x[fnum:fnum+size_batch], dummies_train_y[fnum:fnum+size_batch])
+            model.train(train_x[fnum:fnum + size_batch], dummies_train_y[fnum:fnum + size_batch])
+        print(time.time() - t)
         if epoch % 10 == 0:
             print('\nEpoch %d' % (epoch))
             accuary(model, train_x, train_y, 'Train')
@@ -67,7 +74,11 @@ def train_loop(model, train_x, train_y, test_x, test_y, max_epochs=300):
     PlotConfusionMatrix(model, train_x, train_y, test_x, test_y)
     return
 
-model=Model('Handwritten Digits')
-train_x, train_labels, test_x, test_labels=read_data()
+
+device = "cpu"
+# device = "cuda:0"  # uncomment for running on gpu
+model = Model('Handwritten Digits', device)
+
+train_x, train_labels, test_x, test_labels = read_data()
 train_loop(model, train_x, train_labels, test_x, test_labels)
 ShowExamples(model, test_x, test_labels, 10)
